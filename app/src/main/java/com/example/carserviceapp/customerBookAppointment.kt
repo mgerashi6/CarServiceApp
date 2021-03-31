fac6d69bef6c519f21fd23b0e684a17d8fc74e10
filipbozicevic <74906541+filipbozicevic@users.noreply.github.com>
2021-03-31 16:11:55 +0200

    Added the book appointment and appointments booked pages.

--- app/src/main/java/com/example/carserviceapp/customerBookAppointment.kt
+++ app/src/main/java/com/example/carserviceapp/customerBookAppointment.kt
@@ -3,14 +3,9 @@ package com.example.carserviceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
+class customerBookAppointment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_book_appointment)
-
     }
-
-
 }
-
