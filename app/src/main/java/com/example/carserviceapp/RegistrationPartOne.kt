3859eeba40cb269bed89fe6d1cc474295d340e1e
Mohammad Mogholi Gerashi <mohammad.gerashi@northumbria.ac.uk>
2021-04-01 03:01:49 +0430

    Previous RegistrationSec1 wasn't working so created a new version. Dont use numbers in names/classes/methods

---
+++ app/src/main/java/com/example/carserviceapp/RegistrationPartOne.kt
@@ -0,0 +1,106 @@
+package com.example.carserviceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registration_part_one.*
+
+
+val dropDownListGender = arrayOf("Gender", "Male","Female")
+val dropDownListCountry = arrayOf("Country", "Qatar")
+val dropDownListCity = arrayOf("City", "Doha")
+var selectedGender = ""
+var selectedCountry = ""
+var selectedCity = ""
+
+var lastName = ""
+var mobile = ""
+var nationalID = ""
+var email = ""
+var password = ""
+class RegistrationPartOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_part_one)
+
+        //Gender Select
+        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item, dropDownListGender)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        genderSelect.adapter = adapter
+        genderSelect.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                if(genderSelect.selectedItemPosition ==1){
+                    condition1()
+                }
+                if(genderSelect.selectedItemPosition==2){
+                    selectedGender = "Male"
+                }
+                if(genderSelect.selectedItemPosition==3){
+                    selectedGender = "Female"
+                }
+            }
+        }
+
+        //Country Select
+        val adapter1 = ArrayAdapter(this,android.R.layout.simple_spinner_item, dropDownListCountry)
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        countrySelect.adapter = adapter1
+        countrySelect.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                if(countrySelect.selectedItemPosition == 1){
+                    condition1()
+                }
+                if(countrySelect.selectedItemPosition==2){
+                    selectedCountry = "Qatar"
+                }
+            }
+        }
+
+
+        //City Select
+        val adapter2 = ArrayAdapter(this,android.R.layout.simple_spinner_item, dropDownListCity)
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        citySelect.adapter = adapter2
+        citySelect.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                if(citySelect.selectedItemPosition ==1){
+                    condition1()
+                }
+                if(citySelect.selectedItemPosition==2){
+                    selectedCity = "Doha"
+                }
+            }
+        }
+
+
+        button.setOnClickListener{
+            val checkedButton = checkLegal.isChecked
+            val firstName = findViewById<EditText>(R.id.first_name)
+        }
+    }
+    private fun condition1(){
+        Toast.makeText(this,"Please select a correct option", Toast.LENGTH_LONG).show()
+    }
+}
