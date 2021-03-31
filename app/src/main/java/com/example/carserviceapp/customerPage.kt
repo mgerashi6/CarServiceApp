697b56ad63c9fd5c985d7f024abf49bab829f4f0
filipbozicevic <74906541+filipbozicevic@users.noreply.github.com>
2021-03-31 14:28:43 +0200

    Added the customer page.

---
+++ app/src/main/java/com/example/carserviceapp/customerPage.kt
@@ -0,0 +1,11 @@
+package com.example.carserviceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class customerPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_page)
+    }
+}
