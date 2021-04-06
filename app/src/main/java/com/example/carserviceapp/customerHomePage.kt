006ef500ba43eccc8d417b5e7e0828428bf64ccb
Mohammad Mogholi Gerashi <mohammad.gerashi@northumbria.ac.uk>
2021-04-07 01:05:49 +0430

    Customer Home page is Navigation bar directing to the 3 Fragments, Home page, Profile and Call page

--- app/src/main/java/com/example/carserviceapp/customerHomePage.kt
+++ app/src/main/java/com/example/carserviceapp/customerHomePage.kt
@@ -2,10 +2,38 @@ package com.example.carserviceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.carserviceapp.fragments.CustomerCallFragment
+import com.example.carserviceapp.fragments.CustomerHomeFragment
+import com.example.carserviceapp.fragments.CustomerProfileFragment
+import kotlinx.android.synthetic.main.activity_customer_home_page.*
 
 class customerHomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_home_page)
+
+        val CustomerhomeFragment = CustomerHomeFragment()
+        val CustomerProfileFragment = CustomerProfileFragment()
+        val CustomerCallFragment = CustomerCallFragment()
+
+        makeCurrentFragment(CustomerhomeFragment)
+
+        bottom_nav.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(CustomerhomeFragment)
+                R.id.ic_account -> makeCurrentFragment(CustomerProfileFragment)
+                R.id.ic_call -> makeCurrentFragment(CustomerCallFragment)
+            }
+            true
+        }
     }
+
+        private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
+
 }
