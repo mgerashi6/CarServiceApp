6ed0b69911918d30095c31a88d1ad3f75d9603d7
BenDefty <47889918+BenDefty@users.noreply.github.com>
2021-04-14 18:03:55 +0100

    Added designs for all 5 pages with Kotlin files. Also added a view (rectangle) that was used in the designs.

---
+++ app/src/main/java/com/example/carserviceapp/cs_home_page.kt
@@ -0,0 +1,11 @@
+package com.example.carserviceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cs_home_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cs_home_page)
+    }
+}
