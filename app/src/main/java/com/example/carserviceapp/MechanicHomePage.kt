f2b475e8b7ad416eb88716cfda339c1b8bad6400
Brandon-Chestnutt <w18030237@northumbria.ac.uk>
2021-04-28 21:33:50 +0100

    Mechanic home page

---
+++ app/src/main/java/com/example/carserviceapp/MechanicHomePage.kt
@@ -0,0 +1,20 @@
+package com.example.carserviceapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_mechanic_home_page.*
+
+class MechanicHomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mechanic_home_page)
+
+
+
+        WorkOnCar.setOnClickListener {
+            val intent = Intent(this, UpdateCarReportActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
