1f327fd2aa69c79d644ee9d03630e09fc112330d
BenDefty <47889918+BenDefty@users.noreply.github.com>
2021-04-29 15:22:38 +0100

    Added functionality to the button on the home page.

--- app/src/main/java/com/example/carserviceapp/cs_home_page.kt
+++ app/src/main/java/com/example/carserviceapp/cs_home_page.kt
@@ -1,11 +1,18 @@
 package com.example.carserviceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cs_home_page.*
 
 class cs_home_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cs_home_page)
+
+        HPSearch.setOnClickListener {
+            val intent = Intent(this, UpdateCarReportActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
